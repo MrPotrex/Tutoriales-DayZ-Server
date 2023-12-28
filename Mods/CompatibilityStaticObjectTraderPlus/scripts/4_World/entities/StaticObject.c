5e85541572cee6e76a77de64db7365b3cc400e4d
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2023-12-28 22:44:29 +0100

    Update StaticObject.c

--- Mods/CompatibilityStaticObjectTraderPlus/scripts/4_World/entities/StaticObject.c
+++ Mods/CompatibilityStaticObjectTraderPlus/scripts/4_World/entities/StaticObject.c
@@ -1,1 +1,136 @@
+/*Here is where you need to create your class according to the classname of the static object you want to make compatible with traderplus*/
 
+/*Replace CLASSNAME with your static object classname*/
+
+/*ZT_Vending*/
+class ZT_Vending_Clothing extends BuildingBase{};
+class ZT_Vending_MilitaryClothing extends BuildingBase{};
+class ZT_Vending_Vehicles extends BuildingBase{};
+class ZT_Vending_Aircraft extends BuildingBase{};
+class ZT_Vending_Boats extends BuildingBase{};
+class ZT_Vending_Weapons extends BuildingBase{};
+class ZT_Vending_Attachments extends BuildingBase{};
+class ZT_Vending_Donator extends BuildingBase{};
+class ZT_Vending_Consume extends BuildingBase{};
+class ZT_Vending_Tools extends BuildingBase{};
+class ZT_Vending_BaseStuff extends BuildingBase{};
+class ZT_Vending_Drugs extends BuildingBase{};
+class ZT_Vending_BlackMarket extends BuildingBase{};
+class ZT_Vending_Event extends BuildingBase{};
+class ZT_Vending_Parking extends BuildingBase{};
+class ZT_Vending_Hunting extends BuildingBase{};
+class ZT_Vending_Crafting extends BuildingBase{};
+class ZT_Vending_AttachmentsOnly extends BuildingBase{};
+class ZT_Vending_Ammunition extends BuildingBase{};
+class ZT_Vending_FoodDrink extends BuildingBase{};
+class ZT_Vending_Medic extends BuildingBase{};
+class ZT_Vending_Handguns extends BuildingBase{};
+class ZT_Vending_Melee extends BuildingBase{};
+class ZT_Vending_Misc extends BuildingBase{};
+class ZT_Vending_Raiding extends BuildingBase{};
+class ZT_Vending_Trophy extends BuildingBase{};
+class ZT_Vending_ToxicZone extends BuildingBase{};
+class ZT_Vending_Cannibal extends BuildingBase{};
+
+/*ZT_Vending_Rusty*/
+class ZT_Vending_Clothing_rusty extends BuildingBase{};
+class ZT_Vending_MilitaryClothing_rusty extends BuildingBase{};
+class ZT_Vending_Vehicles_rusty extends BuildingBase{};
+class ZT_Vending_Aircraft_rusty extends BuildingBase{};
+class ZT_Vending_Boats_rusty extends BuildingBase{};
+class ZT_Vending_Weapons_rusty extends BuildingBase{};
+class ZT_Vending_Attachments_rusty extends BuildingBase{};
+class ZT_Vending_Donator_rusty extends BuildingBase{};
+class ZT_Vending_Consume_rusty extends BuildingBase{};
+class ZT_Vending_Tools_rusty extends BuildingBase{};
+class ZT_Vending_BaseStuff_rusty extends BuildingBase{};
+class ZT_Vending_Drugs_rusty extends BuildingBase{};
+class ZT_Vending_BlackMarket_rusty extends BuildingBase{};
+class ZT_Vending_Event_rusty extends BuildingBase{};
+class ZT_Vending_Parking_rusty extends BuildingBase{};
+class ZT_Vending_Hunting_rusty extends BuildingBase{};
+class ZT_Vending_Crafting_rusty extends BuildingBase{};
+class ZT_Vending_AttachmentsOnly_rusty extends BuildingBase{};
+class ZT_Vending_Ammunition_rusty extends BuildingBase{};
+class ZT_Vending_FoodDrink_rusty extends BuildingBase{};
+class ZT_Vending_Medic_rusty extends BuildingBase{};
+class ZT_Vending_Handguns_rusty extends BuildingBase{};
+class ZT_Vending_Melee_rusty extends BuildingBase{};
+class ZT_Vending_Misc_rusty extends BuildingBase{};
+class ZT_Vending_Raiding_rusty extends BuildingBase{};
+class ZT_Vending_Trophy_rusty extends BuildingBase{};
+class ZT_Vending_ToxicZone_rusty extends BuildingBase{};
+class ZT_Vending_Cannibal_rusty extends BuildingBase{};
+
+/*ZT_Vending_Glow*/
+class ZT_Vending_Glow_Clothing extends BuildingBase{};
+class ZT_Vending_Glow_MilitaryClothing extends BuildingBase{};
+class ZT_Vending_Glow_Vehicles extends BuildingBase{};
+class ZT_Vending_Glow_Boats extends BuildingBase{};
+class ZT_Vending_Glow_Aircraft extends BuildingBase{};
+class ZT_Vending_Glow_Weapons extends BuildingBase{};
+class ZT_Vending_Glow_AttachmentsAmmo extends BuildingBase{};
+class ZT_Vending_Glow_Donator extends BuildingBase{};
+class ZT_Vending_Glow_Consume extends BuildingBase{};
+class ZT_Vending_Glow_Tools extends BuildingBase{};
+class ZT_Vending_Glow_BaseStuff extends BuildingBase{};
+class ZT_Vending_Glow_Drugs extends BuildingBase{};
+class ZT_Vending_Glow_BlackMarket extends BuildingBase{};
+class ZT_Vending_Glow_Event extends BuildingBase{};
+class ZT_Vending_Glow_Parking extends BuildingBase{};
+class ZT_Vending_Glow_Hunting extends BuildingBase{};
+class ZT_Vending_Glow_Crafting extends BuildingBase{};
+class ZT_Vending_Glow_Attachments extends BuildingBase{};
+class ZT_Vending_Glow_Ammunition extends BuildingBase{};
+class ZT_Vending_Glow_FoodDrink extends BuildingBase{};
+class ZT_Vending_Glow_Medic extends BuildingBase{};
+class ZT_Vending_Glow_Handguns extends BuildingBase{};
+class ZT_Vending_Glow_Melee extends BuildingBase{};
+class ZT_Vending_Glow_Misc extends BuildingBase{};
+class ZT_Vending_Glow_Raiding extends BuildingBase{};
+class ZT_Vending_Glow_Trophy extends BuildingBase{};
+class ZT_Vending_Glow_ToxicZone extends BuildingBase{};
+class ZT_Vending_Glow_Cannibal extends BuildingBase{};
+
+/*ZT_Vending_Glow V.Rusty*/
+class ZT_Vending_Glow_Clothing_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_MilitaryClothing_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Vehicles_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Boats_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Aircraft_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Weapons_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_AttachmentsAmmo_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Donator_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Consume_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Tools_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_BaseStuff_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Drugs_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_BlackMarket_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Event_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Parking_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Hunting_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Crafting_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Attachments_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Ammunition_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_FoodDrink_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Medic_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Handguns_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Melee_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Misc_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Raiding_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Trophy_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_ToxicZone_Rusty extends BuildingBase{};
+class ZT_Vending_Glow_Cannibal_Rusty extends BuildingBase{};
+
+/*if you've got more than one, just copy paste the line above and change the classname each time according to your object name*/
+
+/*example for noob:
+
+class CLASSNAME1 extends BuildingBase{};
+class CLASSNAME2 extends BuildingBase{};
+class CLASSNAME3 extends BuildingBase{};
+class CLASSNAME4 extends BuildingBase{};
+
+REPLACE CLASSNAME WITH YOUR SPECIFIC ITEM NAME
+
+*/
